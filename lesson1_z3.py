594b22c77ed2ca14e87146731978fa052d5a4e02
Artur matos <67583930+matos85@users.noreply.github.com>
2020-09-23 11:19:13 +0300

    задача 3

---
+++ lesson1_z3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 11:18:47 2020
+
+@author: Матосян
+"""
+
+first = input('Введите число: ')
+result= int(first)+int((first+first))+int((first+first+first))
+print(result)
