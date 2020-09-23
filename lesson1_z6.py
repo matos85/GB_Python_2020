5542dd69a6fe5856939b2c8c688d250d4e8b28d5
Artur matos <67583930+matos85@users.noreply.github.com>
2020-09-23 20:51:19 +0300

    Update lesson1_z6.py

--- lesson1_z6.py
+++ lesson1_z6.py
@@ -8,7 +8,8 @@ Created on Wed Sep 23 14:53:29 2020
 start_result = int(input('Сколько километров пробежал в первый день? '))
 target_result = int(input('Укажите целевое растояние: '))
 result_day = start_result
-dai = 0
+dai = 1
+print(f'{dai}-й день: {"%.0f" % (start_result)}')
 while True:
     dai += 1
     result_day = result_day + (result_day * (10 / 100))
@@ -16,3 +17,4 @@ while True:
     if result_day > target_result:
         print('На {} день спортсмен достиг результата - не менее {} км'.format(dai, "%.0f" % (result_day)))
         break
+
