4c443bcdc7ba35c4d5e814ae57e209c78af311a2
Artur matos <67583930+matos85@users.noreply.github.com>
2020-10-01 09:59:44 +0300

    Update lesson3_z2.py

--- lesson3_z2.py
+++ lesson3_z2.py
@@ -1,4 +1,4 @@
-def data_user(name, surname, age, sity, email, tel_number):
+def data_user(names, surnames, ages, sitys, emails, tel_numbers):
     print(
         f'Имя: {name}. Фамииля: {surname}.Год рождения:{age}. Город: {sity}. Email: {email}. Номер телефона: {tel_number} ')
 
@@ -11,4 +11,4 @@ for i in info_user:
     sity = info_user[3]
     email = info_user[4]
     tel_number = info_user[5]
-data_user(name, surname, age, sity, email, tel_number)
+data_user(names=name, surnames=surname, ages=age, sitys=sity, emails=email, tel_numbers=tel_number)
